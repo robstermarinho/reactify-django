86b933a384eadc202d5dedbb0b6babc53a564e4d
Robert Marinho <robstermarinho@gmail.com>
2020-09-20 12:07:08 -0300

    Login and Logout

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,15 +1,23 @@
 import React from "react";
 import image from "../assets/img/31253.jpg";
-// import { Container } from './styles';
+import { Container } from "./App.css";
 
 function App() {
   return (
-    <>
+    <div className="content">
       <br />
-      <img width={500} src={image} />
+      <img width={800} src={image} />
 
-      <h1>Django + React + Webpack + Babel = Awesome App!!!</h1>
-    </>
+      <h1>Basic Setup</h1>
+      <ul>
+        <li>Django</li>
+        <li>Django + Authentication</li>
+        <li>React</li>
+        <li>Webpack</li>
+        <li>Babel</li>
+      </ul>
+      <a href="accounts/logout">Logout</a>
+    </div>
   );
 }
 
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import App from "./App";
-import "./App.css";
+import "../assets/css/styles.css";
 
 // Render
 render(<App />, document.getElementById("react-app"));
