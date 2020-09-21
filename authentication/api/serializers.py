31e65c92c7ad364e52f8bde9aabe093478374091
Robert Marinho <robstermarinho@gmail.com>
2020-09-20 23:13:19 -0300

    JWT Auth - Obtain token, Refresh token, Create user

--- authentication/api/serializers.py
+++ authentication/api/serializers.py
@@ -1,12 +1,79 @@
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.serializers import TokenObtainSerializer
+from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from authentication.models import CustomUser
 
 
-class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+class TokenObtainPairSerializer(TokenObtainSerializer):
+    """
+    Get user information with token
+    """
 
     @classmethod
     def get_token(cls, user):
-        token = super(MyTokenObtainPairSerializer, cls).get_token(user)
+        return RefreshToken.for_user(user)
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        refresh = self.get_token(self.user)
+
+        data['refresh'] = str(refresh)
+        data['access'] = str(refresh.access_token)
+        data['username'] = self.user.get_username()
+        data['name'] = self.user.get_full_name()
+        data['email'] = self.user.email
+
+        return data
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    @classmethod
+    def get_token(cls, user):
+        token = super(CustomTokenObtainPairSerializer, cls).get_token(user)
 
         # Add custom claims
         token['email'] = user.email
         return token
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    """
+    Currently unused in preference of the below.
+    """
+    email = serializers.EmailField(
+        required=True
+    )
+    username = serializers.CharField()
+    password = serializers.CharField(min_length=8, write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)  # as long as the fields are the same, we can just use this
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
+    def validate_username(self, value):
+        """
+        Check if username has already been taken
+        """
+        if CustomUser.objects.filter(username=value).count() > 0:
+            raise serializers.ValidationError("This username has already been taken.")
+        return value
+
+    def validate_email(self, value):
+        """
+        Check if email has already been taken
+        """
+        if CustomUser.objects.filter(email=value).count() > 0:
+            raise serializers.ValidationError("This email has already been taken.")
+        return value
--- authentication/views.py
+++ authentication/views.py
@@ -1,9 +1,23 @@
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework import permissions
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from authentication.api.serializers import MyTokenObtainPairSerializer
+from authentication.api.serializers import CustomTokenObtainPairSerializer, CustomUserSerializer
 
 
-class ObtainTokenPairWithColorView(TokenObtainPairView):
+class ObtainTokenPairWithEmailView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+
+class CustomUserCreate(APIView):
     permission_classes = (permissions.AllowAny,)
-    serializer_class = MyTokenObtainPairSerializer
+
+    def post(self, request):
+        serializer = CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                json = serializer.data
+                return Response(json, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
